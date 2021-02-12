a45197603f6cb43b3ea029197a21e114d3b2045f
jossalae <jossalase@alum.us.es>
2021-02-12 10:37:37 +0000

    aergaerg

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -164,6 +164,8 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+API = {}
+
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
@@ -183,3 +185,5 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+import django_heroku
+django_heroku.settings(locals())
